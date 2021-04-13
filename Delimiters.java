8902ea683abd6a309ff8eb0af7cb9d0b59cc7b91
Ryan Paik <>
2021-04-13 16:04:08 +0000

    getDelimitersList method and isBalanced method

--- Delimiters.java
+++ Delimiters.java
@@ -13,11 +13,28 @@ public class Delimiters
 
 	public ArrayList<String> getDelimitersList(String[] tokens)
 	{
-		return null;
+    ArrayList<String> delimiters = new ArrayList<String>();
+    for(String str : tokens)
+      if(str.equals(openDel) || str.equals(closeDel))
+        delimiters.add(str);
+
+		return delimiters;
 	}
 
 	public boolean isBalanced(ArrayList<String> delimiters)
 	{ 
-		return false;
+    int openNum = 0, closedNum = 0;
+    for(String str : delimiters)
+    {
+      if(str.equals(openDel))
+        openNum++;
+
+      if(str.equals(closeDel))
+        closedNum++;
+
+      if(closedNum > openNum)
+        return false;
+    }
+    return openNum == closedNum;
 	}
 }
